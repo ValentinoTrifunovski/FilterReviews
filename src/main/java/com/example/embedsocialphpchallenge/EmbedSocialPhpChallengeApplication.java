e1c4049c1f90f75b31db5ba7a9483a809bad55d8
ValentinoTrifunovski <valentino.trifunovski@gmail.com>
2023-03-16 16:03:39 +0100

    First Commit

---
+++ src/main/java/com/example/embedsocialphpchallenge/EmbedSocialPhpChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.embedsocialphpchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedSocialPhpChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbedSocialPhpChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/embedsocialphpchallenge/EmbedSocialPhpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embedsocialphpchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedSocialPhpChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
